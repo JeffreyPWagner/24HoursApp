cba3743a9bb5e71352620f6b4b0b2c4f75691115
JeffreyPWagner <41526084+JeffreyPWagner@users.noreply.github.com>
2019-01-29 18:05:16 -0500

    Timer starts from previous time

--- app/src/main/java/me/jeffreywagner/a24hours/MainActivity.kt
+++ app/src/main/java/me/jeffreywagner/a24hours/MainActivity.kt
@@ -1,11 +1,11 @@
 package me.jeffreywagner.a24hours
 
 import android.os.Bundle
+import android.os.SystemClock
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Button
 import android.widget.Chronometer
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,23 +14,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val chronometer = findViewById<Chronometer>(R.id.chronometer)
+        var baseTime: Long = 0
 
-        val button = findViewById<Button>(R.id.button)
-        button?.setOnClickListener(object : View.OnClickListener {
+
+        val startButton = findViewById<Button>(R.id.startButton)
+        startButton?.setOnClickListener(object : View.OnClickListener {
 
             internal var isPlaying = false
 
             override fun onClick(v: View) {
                 if (!isPlaying) {
+                    chronometer.base = SystemClock.elapsedRealtime() - baseTime
                     chronometer.start()
                     isPlaying = true
                 } else {
+                    baseTime = SystemClock.elapsedRealtime() - chronometer.base
                     chronometer.stop()
                     isPlaying = false
                 }
 
-                button.setText(if (isPlaying) R.string.start else R.string.stop)
-                Toast.makeText(this@MainActivity, getString(if (isPlaying) R.string.playing else R.string.stopped), Toast.LENGTH_SHORT).show()
+                startButton.setText(if (!isPlaying) R.string.start else R.string.stop)
             }
         })
     }
