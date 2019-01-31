21bb5c79376cb8b4819bae8f65414c51b8e0e897
JeffreyPWagner <41526084+JeffreyPWagner@users.noreply.github.com>
2019-01-30 20:50:33 -0500

    UI prototype built

--- app/src/main/java/me/jeffreywagner/a24hours/MainActivity.kt
+++ app/src/main/java/me/jeffreywagner/a24hours/MainActivity.kt
@@ -2,40 +2,35 @@ package me.jeffreywagner.a24hours
 
 import android.os.Bundle
 import android.os.SystemClock
-import android.support.v7.app.AppCompatActivity
+import android.app.Activity
 import android.view.View
 import android.widget.Button
 import android.widget.Chronometer
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
+
+    private lateinit var chronometer: Chronometer
+    var baseTime = 0L
+    var isPlaying = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val chronometer = findViewById<Chronometer>(R.id.chronometer)
-        var baseTime: Long = 0
-
-
+        chronometer = findViewById(R.id.chronometer)
         val startButton = findViewById<Button>(R.id.startButton)
-        startButton?.setOnClickListener(object : View.OnClickListener {
 
-            internal var isPlaying = false
-
-            override fun onClick(v: View) {
-                if (!isPlaying) {
-                    chronometer.base = SystemClock.elapsedRealtime() - baseTime
-                    chronometer.start()
-                    isPlaying = true
-                } else {
-                    baseTime = SystemClock.elapsedRealtime() - chronometer.base
-                    chronometer.stop()
-                    isPlaying = false
-                }
-
-                startButton.setText(if (!isPlaying) R.string.start else R.string.stop)
+        startButton.setOnClickListener {v ->
+            if (!isPlaying) {
+                chronometer.base = SystemClock.elapsedRealtime() - baseTime
+                chronometer.start()
+                isPlaying = true
+            } else {
+                baseTime = SystemClock.elapsedRealtime() - chronometer.base
+                chronometer.stop()
+                isPlaying = false
             }
-        })
+            startButton.setText(if (!isPlaying) R.string.start else R.string.stop)
+        }
     }
 }
 
